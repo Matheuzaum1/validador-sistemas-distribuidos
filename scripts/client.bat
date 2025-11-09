e10bcf42d754d262f77496ad2b8e527a51bc2128
Matheuzaum1 <matheushenriquerosendo@gmail.com>
2025-11-09 18:19:45 -0300

    refactor: remove arquivos redundantes (.md e .bat)

--- scripts/client.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Iniciando cliente...
-if exist "target\validador-sistemas-distribuidos-1.0.0.jar" (
-    java -cp "target\validador-sistemas-distribuidos-1.0.0.jar" com.distribuidos.client.ClientMain
-) else (
-    echo [ERRO] JAR nao encontrado! Execute build.bat primeiro.
-    pause
-)
--- scripts/server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Iniciando servidor...
-if exist "target\validador-sistemas-distribuidos-1.0.0.jar" (
-    java -cp "target\validador-sistemas-distribuidos-1.0.0.jar" com.distribuidos.server.ServerMain
-) else (
-    echo [ERRO] JAR nao encontrado! Execute build.bat primeiro.
-    pause
-)
